2f30c5a7a54668ed76f887e3b2101d3a96213f82
j-Abban <justice.abban@amalitech.org>
2024-07-23 11:49:33 +0000

    New Changes

---
+++ PageObjects/PageActions/SellerPageActions.js
@@ -0,0 +1,57 @@
+//Seller Registration Page Actions
+
+class RegistrationPage {
+    visit = () => {
+        cy.visit('/auth/seller-signup');
+    }
+
+    fillFirstName = (userfirstname) => {
+        cy.get(':nth-child(1) > .form-group > .w-full').type(userfirstname);
+    }
+
+    fillLastName =(userlastname) => {
+        cy.get(':nth-child(2) > .form-group > .w-full').type(userlastname);
+    }
+
+    fillEmail = (email) => {
+        cy.get(':nth-child(3) > .form-group > .w-full').type(email);
+    }
+
+    clickNextButton = () => {
+        cy.get('button').click();
+    }
+
+    fillLocation = (location) => {
+        cy.get(':nth-child(1) > .form-group > .w-full').type(location);
+    }
+
+    fillPhone = (phone) => {
+        cy.get(':nth-child(2) > .form-group > .w-full').type(phone);
+    }
+
+    fillOrganization = (Organization) => {
+    cy.get(':nth-child(3) > .form-group > .w-full').type(Organization);
+    }
+
+   clickButton = () => {
+    cy.get('.flex > :nth-child(2) > .w-full').click();
+   }
+
+   fillCreatePassword = (password) => {
+    cy.get(':nth-child(1) > .form-group > .w-full').type(password);
+}
+
+fillConfirmPassword = (confirmPassword) => {
+    cy.get(':nth-child(2) > .form-group > .w-full').type(confirmPassword);
+}
+
+checkCheckbox = () => {
+    cy.get('.ng-dirty.ng-touched.ng-valid').check();
+}
+
+clickRegisterButton = () => {
+    cy.get('.flex > :nth-child(2) > .w-full').should('not.bevisible').click({force: true}).should('be.checked');
+}
+}
+
+export default RegistrationPage;
---
+++ cypress/e2e/SellerRegistration.cy.js
@@ -0,0 +1,25 @@
+/// <reference types="cypress" />
+//Seller Account Registration
+import RegistrationPage from "../../PageObjects/PageActions/SellerPageActions";
+
+describe('Registration Page', () => {
+
+const registrationPage = new RegistrationPage();
+
+    it('should register a new user', () => {
+     registrationPage.visit();
+     registrationPage.fillFirstName('Nana');
+     registrationPage.fillLastName('Kwame');
+     registrationPage.fillEmail('nana.kwame@example.com');
+     registrationPage.clickNextButton();
+     registrationPage.fillLocation('Takoradi');
+     registrationPage.fillPhone('+233557298173');
+     registrationPage.fillOrganization('Amakom');
+     registrationPage.clickButton();
+     registrationPage.fillCreatePassword('Kw@m3.Jak1');
+     registrationPage.fillConfirmPassword('Kw@m3.Jak1');
+     registrationPage.checkCheckbox();
+     registrationPage.clickRegisterButton();
+    })
+   
+})
