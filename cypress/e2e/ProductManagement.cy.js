1164f97228302759f4888980fd776584d59ad062
j-Abban <justice.abban@amalitech.org>
2024-08-01 10:43:53 +0000

    New

--- cypress/e2e/ProductManagement.cy.js
+++ cypress/e2e/ProductManagement.cy.js
@@ -23,7 +23,7 @@ describe('Full User Flow Test', () => {
      //product.clickOnManageProducts();
      //product.clickAddProduct();
      cy.get(':nth-child(9) > .ng-star-inserted > .nav-item > p').click();
-     cy.get('div.md\\:text-base.text-xs.font-bold.px-4.py-1.md\\:px-7.md\\:py-2.font-sans.bg-primary-100.text-white.rounded-lg.cursor-pointer').click();
+     cy.get("[class='flex w-full items-center justify-between px-4 my-4'] [tabindex]").click();
      
     });
   });
